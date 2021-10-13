e83780dc5c76a43af082fba752fc0290f1e7a702
Pedro Albanese <68971450+pedroalbanese@users.noreply.github.com>
2021-10-13 16:15:10 -0300

    Update main.go

--- cmd/vko/main.go
+++ cmd/vko/main.go
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-// Command-line 34.10-2012 Public key shared key agreement.
+// Command-line 34.10-2012 Shared key agreement (VKO).
 package main
 
 import (
