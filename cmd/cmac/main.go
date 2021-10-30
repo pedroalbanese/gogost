24c62982a9718874583778dc919a819c7768d6bf
Pedro Albanese <68971450+pedroalbanese@users.noreply.github.com>
2021-10-30 18:39:07 -0300

    Update main.go

--- cmd/cmac/main.go
+++ cmd/cmac/main.go
@@ -1,3 +1,20 @@
+// GoGOST -- Pure Go GOST cryptographic functions library
+// Copyright (C) 2015-2020 Sergey Matveev <stargrave@stargrave.org>
+// Copyright (C) 2020-2021 Pedro Albanese <pedroalbanese@hotmail.com>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, version 3 of the License.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+// Command-line 28147-89 CMAC function.
 package main
 
 import (
